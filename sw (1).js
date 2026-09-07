de794a763a2550892ca09b9e932ebcfe56e84b95
s29321333-lang <s29321333@gmail.com>
2026-09-06 21:57:07 -0400

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "purgatory-scan-v5";
+self.addEventListener("install", event => self.skipWaiting());
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match(event.request))
+  );
+});
